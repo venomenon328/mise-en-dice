20852987767015c56707974c63a837f79fccac36
venomenon328 <tobias@venomenon.de>
2026-08-13 19:47:51 +0200

    Allow administration to use challenge API

--- src/main/java/io/github/venomenon328/miseendice/administration/package-info.java
+++ src/main/java/io/github/venomenon328/miseendice/administration/package-info.java
@@ -1,5 +1,5 @@
 @org.springframework.modulith.ApplicationModule(
         displayName = "Administration",
-        allowedDependencies = "catalog :: api"
+        allowedDependencies = {"catalog :: api", "challenge :: api"}
 )
 package io.github.venomenon328.miseendice.administration;
